e0a5adbb87cacb86937c8da09a7192ac7f0278c0
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-08-31 12:18:42 +0200

    Updating and Testing Dependencies

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import "bootstrap"
-import {library} from "@fortawesome/fontawesome-svg-core"
-import "animate.css/animate.css"
+import 'bootstrap'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-import "bootstrap/dist/css/bootstrap.css"
+import 'animate.css/animate.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 import {
-    faShoppingCart,
-    faDollarSign
-} from "@fortawesome/free-solid-svg-icons"
+  faShoppingCart,
+  faDollarSign
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faShoppingCart,faDollarSign)
+library.add(faShoppingCart, faDollarSign)
 
 createApp(App).mount('#app')
