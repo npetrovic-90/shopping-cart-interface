5b4e0eb9a56b59ffcebbc7400dba362b1a9daca7
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-09-05 11:48:49 +0200

    Added ProductList component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,53 @@
 <template>
   <div id="app" class="container mt-5">
-      <h1>My Shop</h1>
-      <p class="animate__animated animate__fadeInRight">Take a loot at our offerings</p>
-      <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-      <price value="4.52"></price>
+    <h1>My Shop</h1>
+    <product-list :maximum="maximum" :products="products" @add="addItem"> </product-list>
   </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import Price from './components/Price.vue'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import ProductList from './components/ProductList.vue'
 
 export default {
   name: 'App',
+  data: function () {
+    return {
+      maximum: 99,
+      cart: [],
+      products: null
+    }
+  },
+  methods: {
+    addItem: function (product) {
+      var whichProduct
+      var existing = this.cart.filter(function (item, index) {
+        if (item.product.id === Number(product.id)) {
+          whichProduct = index
+          return true
+        } else {
+          return false
+        }
+      })
+
+      if (existing.length) {
+        this.cart[whichProduct].qty++
+      } else {
+        this.cart.push({ product: product, qty: 1 })
+      }
+    }
+  },
   components: {
-    FontAwesomeIcon,
-    Price
+    // FontAwesomeIcon,
+    ProductList
+  },
+  mounted: function () {
+    fetch('https://hplussport.com/api/products/order/price')
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data
+      })
   }
 }
 </script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,65 @@
+<template>
+  <transition-group
+    name="fade"
+    tag="div"
+    @beforeEnter="beforeEnter"
+    @enter="enter"
+    @leave="leave"
+  >
+    <div
+      class="row d-none mb-3 align-items-center"
+      v-for="(item, index) in products"
+      :key="item.id" v-bind:data-index="index"
+    >
+      <div class="col-1 m-auto">
+        <button class="btn btn-info" v-on:click="$emit('add', item)">+</button>
+      </div>
+
+      <div class="col-sm-4">
+        <img
+          class="img-fluid d-block"
+          height="100"
+          width="300"
+          :src="item.image"
+          :alt="item.name"
+        />
+      </div>
+      <div class="col">
+        <h3 class="text-info">{{ item.name }}</h3>
+        <p class="mb-0">{{ item.description }}</p>
+        <div class="h5 float-end">
+          <price :value="Number(item.price)"></price>
+        </div>
+      </div>
+    </div>
+  </transition-group>
+</template>
+
+<script>
+import Price from './Price.vue'
+
+export default {
+  name: 'product-list',
+  components: { Price },
+  props: ['products', 'maximum'],
+  methods: {
+    beforeEnter: function (el) {
+      el.className = 'd-none'
+    },
+    enter: function (el) {
+      var delay = el.dataset.index * 100
+      setTimeout(function () {
+        el.className =
+          'row d-flex mb-3 align-items-center animate__animated animate__fadeInRight'
+      }, delay)
+    },
+    leave: function (el) {
+      var delay = el.dataset.index * 100
+      setTimeout(function () {
+        el.className =
+          'row d-flex mb-3 align-items-center animate__animated animate__fadeOutRight'
+      }, delay)
+    }
+  }
+}
+</script>
