f3d908d23203f3dc6c63c785730c97391f9d1bf6
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-09-05 11:07:52 +0200

    Added Price component

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
       <h1>My Shop</h1>
       <p class="animate__animated animate__fadeInRight">Take a loot at our offerings</p>
       <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+      <price value="4.52"></price>
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Price from './components/Price.vue'
 
 export default {
   name: 'App',
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+    Price
   }
 }
 </script>
